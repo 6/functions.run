994fb299958fca8d3ebb7a17c51540e5a69f50c4
Peter Graham <peterghm@gmail.com>
2016-10-23 16:34:10 -0400

    better page titles / descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     @view_flow.set(:layout, output_buffer)
     self.output_buffer = render(file: "layouts/#{layout}")
   end
+
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
+  def description(description)
+    content_for(:description) { description }
+  end
 end
